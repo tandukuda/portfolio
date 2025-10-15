3b3e582b285ffb05cd064daff01f36cc3a0e4cbd
Ridho Ibrahim <133313292+tandukuda@users.noreply.github.com>
2025-10-15 17:05:36 +0700

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -99,7 +99,8 @@ const about: About = {
     description: (
       <>
         tandukuda is a 3D artist based in the bustling city of Surabaya, Indonesia, known for bringing hauntingly atmospheric visuals to life. From stage visuals and live productions to music videos and NFTs, tandukuda's work spans diverse media, each piece immersing the audience in an unforgettable, eerie experience.
-      </>
+
+Driven by a relentless passion for innovation, tandukuda explores avant-garde techniques to create dark and captivating worlds that push the boundaries of digital design. With a unique style that merges horror and immersive storytelling, tandukuda’s work is crafted to leave a lasting impact, engaging viewers and sparking inspiration in those who encounter his haunting art.
     ),
   },
   work: {
